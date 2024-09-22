0ab7a93d3744dbeef44d6d77e014d18c3dc5a144
vkarchevskyi <vkarchevskyi10@gmail.com>
2024-09-22 16:46:45 +0300

    feat(scores): create routing for app.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ScoreController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ScoreController::class, 'index'])->name('score.index');
+Route::get('/{user}', [ScoreController::class, 'indexUser'])->name('score.user');
+Route::post('/add', [ScoreController::class, 'create'])
+    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->name('score.create');
